a5ba49e85d80722caa498570bd5f6a31ad7f48af
Jon West <electrikjesus@gmail.com>
2020-04-21 10:16:45 -0400

    Remove kernel building for now
    Change-Id: I85b91270f80dd5c93b832378720f461e5246932e

--- config/BoardConfigBliss.mk
+++ config/BoardConfigBliss.mk
@@ -3,9 +3,7 @@ ifeq ($(WITH_BLISS_CHARGER),true)
     BOARD_HAL_STATIC_LIBRARIES := libhealthd.bliss
 endif
 
-ifneq ($(TARGET_PC_BUILD),true)
-include vendor/bliss/config/BoardConfigKernel.mk
-endif
+#include vendor/bliss/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/bliss/config/BoardConfigQcom.mk
