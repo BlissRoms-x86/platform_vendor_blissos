ca7c757309ce385ac2e5f21e8c9da2ad834581c8
Jackeagle <jackeagle102@gmail.com>
2020-04-21 08:19:42 +0200

    Revert "vendor/bliss: Kill Accents"
    This reverts commit 4f7d32089de14d68d8323ad6d045677b0145a3b0.

--- config/bliss_packages.mk
+++ config/bliss_packages.mk
@@ -70,6 +70,19 @@ PRODUCT_PACKAGES += \
     OmniJaws \
     OmniStyle
 
+# Accents
+PRODUCT_PACKAGES += \
+    AccentColorYellowOverlay \
+    AccentColorVioletOverlay \
+    AccentColorTealOverlay \
+    AccentColorRedOverlay \
+    AccentColorQGreenOverlay \
+    AccentColorPinkOverlay \
+    AccentColorLightPurpleOverlay \
+    AccentColorIndigoOverlay \
+    AccentColorFlatPinkOverlay \
+    AccentColorCyanOverlay \
+    AccentColorBlueGrayOverlay
 
 # OmniJaws Permission
 PRODUCT_COPY_FILES += \
